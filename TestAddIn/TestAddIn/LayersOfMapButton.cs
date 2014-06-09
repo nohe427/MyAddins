3a24e3ba6836a855c0b57524fa2cc22bdcac883f
Alexander Nohe <alex.nohe427@gmail.com>
2014-06-08 19:41:33 -0700

    AddIn Test

---
+++ TestAddIn/TestAddIn/LayersOfMapButton.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using ESRI.ArcGIS.ArcMapUI;
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.Framework;
+
+namespace TestAddIn
+{
+    public class LayersOfMapButton : ESRI.ArcGIS.Desktop.AddIns.Button
+    {
+        public LayersOfMapButton()
+        {
+        }
+
+        protected override void OnClick()
+        {
+            string data = "";
+
+            IDocument doc = ArcMap.Application.Document;
+            IMxDocument mxDoc = doc as IMxDocument;
+
+            IMap map = mxDoc.FocusMap;
+            data += "Using LayerCount Property \n";
+
+            ILayer layer;
+            for (int i = 0; i < map.LayerCount; i++)
+            {
+                layer = map.Layer[i];
+                bool visibility = layer.Visible;
+                data += ">> " + layer.Name[i] + "\n";
+            }
+            layer = null;
+            data += string.Format( "{0} Map contains {1} Layers\n", map.Name, map.LayerCount);
+            data += "---------------------------------------------------------------\n";
+            data += "Using Layer Property \n";
+            IEnumLayer enumLayer = map.Layers;
+            layer = enumLayer.Next();
+            int j = 0;
+            while (layer != null)
+            {
+                j++;
+                data += ">>" + layer.Name + "\n";
+                layer = enumLayer.Next();
+            }
+            data += string.Format("{0} Map Contains {1} Layers", map.Name, j);
+
+            Message msgFrom = new Message();
+            msgFrom.label1.Text = data;
+            msgFrom.ShowDialog();
+        }
+        protected override void OnUpdate()
+        {
+            Enabled = ArcMap.Application != null;
+        }
+    }
+
+}
---
+++ TestAddIn/TestAddIn/Message.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace TestAddIn
+{
+    partial class Message
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(14, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(280, 280);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // Message
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(306, 302);
+            this.Controls.Add(this.label1);
+            this.Name = "Message";
+            this.Text = "Message";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Label label1;
+
+    }
+}
