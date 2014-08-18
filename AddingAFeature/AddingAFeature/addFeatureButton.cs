a3ed36834d439f2d3ef0094b494fb31300420e44
Alexander Nohe <alex.nohe427@gmail.com>
2014-08-17 20:03:31 -0700

    Add a feature class to a map sample.
    The feature class is empty but it does get added.

---
+++ AddingAFeature/AddingAFeature/addFeatureButton.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.DataSourcesGDB;
+using ESRI.ArcGIS.Framework;
+using ESRI.ArcGIS.Geodatabase;
+using ESRI.ArcGIS.ArcMapUI;
+
+namespace AddingAFeature
+{
+    public class addFeatureButton : ESRI.ArcGIS.Desktop.AddIns.Button
+    {
+        public addFeatureButton()
+        {
+        }
+
+        protected override void OnClick()
+        {
+            //
+            //  TODO: Sample code showing how to access button host
+            //
+            // ArcMap.Application.CurrentTool = null;
+            IDocument doc = ArcMap.Application.Document;
+            IMxDocument mxDoc = doc as IMxDocument;
+            int value = ArcMap.Application.hWnd;
+
+            IActiveView av = mxDoc.ActiveView as IActiveView;
+            IMap map = mxDoc.FocusMap as IMap;
+            
+            IWorkspaceFactory wsf = new FileGDBWorkspaceFactory();
+            IFeatureWorkspace ws = wsf.OpenFromFile("C:\\Users\\AlexanderN\\Documents\\GitHub\\MyAddins\\AddingAFeature\\features.gdb", value) as IFeatureWorkspace;
+            IFeatureLayer featureLayer = new FeatureLayerClass();
+            featureLayer.FeatureClass = ws.OpenFeatureClass("pointFeature");
+            ILayer layer = (ILayer)featureLayer;
+            layer.Name = featureLayer.FeatureClass.AliasName;
+            
+            map.AddLayer(layer);
+            av.Refresh();
+        }
+        protected override void OnUpdate()
+        {
+            Enabled = ArcMap.Application != null;
+        }
+    }
+
+}
