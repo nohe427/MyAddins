864834624256b8e346eb9f71ba708db05dcdbf01
Alexander P Nohe <anohe@esri.com>
2014-08-28 15:29:07 -0700

    Testing Stuffz

---
+++ ProAppModule3/ProAppModule3/Button1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+using ArcGIS.Desktop.Core;
+using ArcGIS.Desktop.Mapping;
+
+namespace ProAppModule3
+{
+    internal class Button1 : Button
+    {
+        protected override void OnClick()
+        {
+            //System.Windows.MessageBox.Show("Hello World!");
+            Project project = ProjectModule.CurrentProject;
+
+            IProjectItemContainer projectContainer = project.GetProjectItemContainer("Map");
+
+            foreach(var item in projectContainer.GetProjectItems())
+            {
+                System.Windows.MessageBox.Show(item.Name);
+                ArcGIS.Desktop.Mapping.Map map = MappingModule.FindMap(item.Path);
+            }
+
+        }
+    }
+}
---
+++ ProAppModule3/ProAppModule3/Module1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+
+namespace ProAppModule3
+{
+    internal class Module1 : Module
+    {
+        private static Module1 _this = null;
+
+        /// <summary>
+        /// Retrieve the singleton instance to this module here
+        /// </summary>
+        public static Module1 Current
+        {
+            get
+            {
+                return _this ?? (_this = (Module1)FrameworkApplication.FindModule("ProAppModule3_Module"));
+            }
+        }
+    }
+}
