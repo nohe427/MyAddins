6ed51ffdc41cbd337a2357f12e163f4f2bf4d334
Alexander Nohe <alex.nohe427@gmail.com>
2015-02-08 19:23:50 -0800

    Chopping it down
    Should be good to go on monday

--- timeCardStopwatch/timeCardStopwatch/TimeCardHelper.cs
+++ timeCardStopwatch/timeCardStopwatch/TimeCardHelper.cs
@@ -31,70 +31,40 @@ namespace timeCardStopwatch
 
         private void caseButton_Click(object sender, EventArgs e)
         {
-            if (premiumCheckBox.Checked == true && internationalCheckBox.Checked == true && caseComboBox.SelectedItem.ToString() == "Desktop Extensions")
+            string part1 = "";
+            string part2 = "";
+            if (premiumCheckBox.Checked == true && internationalCheckBox.Checked == true)
             {
-<<<<<<< HEAD
-                timers.SwitchActiveTimer("Premium Internation Desktop Extensions");
-                timerToolStripStatusLabel.Text = "Currently Running Timer: Premium Internation Desktop Extensions";
+                part1 = "Premium Int ";
             }
-            else if (premiumCheckBox.Checked == false && internationalCheckBox.Checked == true && caseComboBox.SelectedItem.ToString() == "Desktop Extensions")
+            else if (premiumCheckBox.Checked == false && internationalCheckBox.Checked == true)
             {
-                timers.SwitchActiveTimer("Internation Desktop Extensions");
-                timerToolStripStatusLabel.Text = "Currently Running Timer: Internation Desktop Extensions";
-=======
-                StopAllStopWatches();
-                stopwatchCasesPremiumInternationalDTEXT.Start();
-                timerToolStripStatusLabel.Text = "Currently Running Timer: Premium International Desktop Extensions";
+                part1 = "Int ";
             }
-            else if (premiumCheckBox.Checked == false && internationalCheckBox.Checked == true && caseComboBox.SelectedItem.ToString() == "Desktop Extensions")
+            else if (premiumCheckBox.Checked == true && internationalCheckBox.Checked == false)
             {
-                StopAllStopWatches();
-                stopwatchCasesInternationalDTEXT.Start();
-                timerToolStripStatusLabel.Text = "Currently Running Timer: International Desktop Extensions";
->>>>>>> origin/master
+                part1 = "Premiun ";
             }
-            else if (premiumCheckBox.Checked == true && internationalCheckBox.Checked == false && caseComboBox.SelectedItem.ToString() == "Desktop Extensions")
+            else if (premiumCheckBox.Checked == false && internationalCheckBox.Checked == false)
             {
-                timers.SwitchActiveTimer("Premium Desktop Extensions");
-                timerToolStripStatusLabel.Text = "Currently Running Timer: Premium Desktop Extensions";
+                part1 = "Domestic ";
             }
-            else if (premiumCheckBox.Checked == false && internationalCheckBox.Checked == false && caseComboBox.SelectedItem.ToString() == "Desktop Extensions")
+
+            if (caseComboBox.SelectedItem.ToString() == "Desktop Extensions")
             {
-                timers.SwitchActiveTimer("Domestic Desktop Extensions");
-                timerToolStripStatusLabel.Text = "Currently Running Timer: Domestic Desktop Extensions";
+                part2 = "Desktop Extensions";
             }
-            else if (premiumCheckBox.Checked == false && internationalCheckBox.Checked == false && caseComboBox.SelectedItem.ToString() == "Desktop")
+            else if (caseComboBox.SelectedItem.ToString() == "Desktop")
             {
-                timers.SwitchActiveTimer("Domestic Desktop");
-                timerToolStripStatusLabel.Text = "Currently Running Timer: Domestic Desktop";
+                part2 = "Desktop";
             }
-            else if (premiumCheckBox.Checked == false && internationalCheckBox.Checked == true && caseComboBox.SelectedItem.ToString() == "Desktop")
+            else if (caseComboBox.SelectedItem.ToString() == "ArcGIS Online")
             {
-<<<<<<< HEAD
-                timers.SwitchActiveTimer("Internation Desktop");
-                timerToolStripStatusLabel.Text = "Currently Running Timer: Internation Desktop";
+                part2 = "ArcGIS Online";
             }
-            else if (premiumCheckBox.Checked == true && internationalCheckBox.Checked == true && caseComboBox.SelectedItem.ToString() == "Desktop")
-            {
-                timers.SwitchActiveTimer("Premium Internation Desktop");
-                timerToolStripStatusLabel.Text = "Currently Running Timer: Premium Internation Desktop";
-=======
-                StopAllStopWatches();
-                stopwatchCasesInternationalDT.Start();
-                timerToolStripStatusLabel.Text = "Currently Running Timer: International Desktop";
-            }
-            else if (premiumCheckBox.Checked == true && internationalCheckBox.Checked == true && caseComboBox.SelectedItem.ToString() == "Desktop")
-            {
-                StopAllStopWatches();
-                stopwatchCasesPremiumInternationalDT.Start();
-                timerToolStripStatusLabel.Text = "Currently Running Timer: Premium International Desktop";
->>>>>>> origin/master
-            }
-            else if (premiumCheckBox.Checked == true && internationalCheckBox.Checked == false && caseComboBox.SelectedItem.ToString() == "Desktop")
-            {
-                timers.SwitchActiveTimer("Premium Desktop");
-                timerToolStripStatusLabel.Text = "Currently Running Timer: Premium Desktop";
-            }
+            string timerCalled = part1 + part2;
+            timers.SwitchActiveTimer(timerCalled);
+            timerToolStripStatusLabel.Text = "Currently Running Timer: " + timerCalled;
         }
 
         private void collaborationButton_Click(object sender, EventArgs e)
@@ -113,12 +83,12 @@ namespace timeCardStopwatch
         {
             caseComboBox.Items.Add("Desktop");
             caseComboBox.Items.Add("Desktop Extensions");
+            caseComboBox.Items.Add("ArcGIS Online");
             caseComboBox.SelectedIndex = caseComboBox.FindString("Desktop");
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-<<<<<<< HEAD
             
             if(timers.TimersActive.Exists(x => x.Name == "Meetings"))
             {
@@ -133,26 +103,6 @@ namespace timeCardStopwatch
 
             totalTime = timers.AllTimes();
             
-=======
-            tsCasesPremiumInternationalDTEXT = stopwatchCasesPremiumInternationalDTEXT.Elapsed;
-            tsCasesInternationalDTEXT = stopwatchCasesInternationalDTEXT.Elapsed;
-            tsCasesPremiumDTEXT = stopwatchCasesPremiumDomesticDTEXT.Elapsed;
-            tsCasesDTEXT = stopwatchCasesDomesticDTEXT.Elapsed;
-            tsCasesDT = stopwatchCasesDomesticDT.Elapsed;
-            tsCasesPremiumDT = stopwatchCasesPremiumDomesticDT.Elapsed;
-            tsCasesIntDT = stopwatchCasesInternationalDT.Elapsed;
-            tsCasesPremiumIntDT = stopwatchCasesPremiumInternationalDT.Elapsed;
-
-            TimeSpan tsMeetings = stopwatchMeetings.Elapsed;
-            TimeSpan tsCollaboration = stopwatchCollaboration.Elapsed;
-
-            TimeSpan totalSpanCases = tsCasesPremiumInternationalDTEXT + tsCasesInternationalDTEXT + tsCasesPremiumDTEXT + tsCasesDTEXT
-                + tsCasesDT + tsCasesPremiumDT + tsCasesIntDT + tsCasesPremiumIntDT;
-
-            TimeSpan totalTime = tsCasesPremiumInternationalDTEXT + tsCasesInternationalDTEXT + tsCasesPremiumDTEXT + tsCasesDTEXT
-                + tsCasesDT + tsCasesPremiumDT + tsCasesIntDT + tsCasesPremiumIntDT + tsMeetings + tsCollaboration;
-
->>>>>>> origin/master
             caseLabel.Text = String.Format("{0:00}:{1:00}:{2:00}",
             totalSpanCases.Hours, totalSpanCases.Minutes, totalSpanCases.Seconds);
 
@@ -174,24 +124,9 @@ namespace timeCardStopwatch
 
         private void TimeCardHelper_FormClosing(object sender, FormClosingEventArgs e)
         {
-<<<<<<< HEAD
+
             timers.StopAllTimers();
             MessageBox.Show(timers.GetAllTimes());
-=======
-            StopAllStopWatches();
-            MessageBox.Show("TE0352: " + meetingLabel.Text +
-                "\nTE0353: " + collaborationLabel.Text +
-                "\n C04550M0101: " + String.Format("{0:00}:{1:00}:{2:00}", tsCasesDTEXT.Hours, tsCasesDTEXT.Minutes, tsCasesDTEXT.Seconds) +
-                "\n C04558M0101: " + String.Format("{0:00}:{1:00}:{2:00}", tsCasesPremiumInternationalDTEXT.Hours, tsCasesPremiumInternationalDTEXT.Minutes, tsCasesPremiumInternationalDTEXT.Seconds) +
-                "\n C04552M0101: " + String.Format("{0:00}:{1:00}:{2:00}", tsCasesInternationalDTEXT.Hours, tsCasesInternationalDTEXT.Minutes, tsCasesInternationalDTEXT.Seconds) +
-                "\n C04552M0101: " + String.Format("{0:00}:{1:00}:{2:00}", tsCasesPremiumDTEXT.Hours, tsCasesPremiumDTEXT.Minutes, tsCasesPremiumDTEXT.Seconds) +
-                "\n C04550M0100: " + String.Format("{0:00}:{1:00}:{2:00}", tsCasesDT.Hours, tsCasesDT.Minutes, tsCasesDT.Seconds) +
-                "\n C04552M0100: " + String.Format("{0:00}:{1:00}:{2:00}", tsCasesPremiumDT.Hours, tsCasesPremiumDT.Minutes, tsCasesPremiumDT.Seconds) +
-                "\n C04551M0100: " + String.Format("{0:00}:{1:00}:{2:00}", tsCasesIntDT.Hours, tsCasesIntDT.Minutes, tsCasesIntDT.Seconds) +
-                "\n C04558M0100: " + String.Format("{0:00}:{1:00}:{2:00}", tsCasesPremiumIntDT.Hours, tsCasesPremiumIntDT.Minutes, tsCasesPremiumIntDT.Seconds) +
-
-                "\n\n Total Time: " + totalTimeLabel.Text);
->>>>>>> origin/master
         }
 
         private void lunchButton_Click(object sender, EventArgs e)
