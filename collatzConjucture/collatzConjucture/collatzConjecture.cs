3f4885735c8e280fd7c50b67af6057db74928335
Alexander Nohe <alex.nohe427@gmail.com>
2015-02-19 20:28:03 -0800

    Finished the project.

--- collatzConjucture/collatzConjucture/collatzConjecture.cs
+++ collatzConjucture/collatzConjucture/collatzConjecture.cs
@@ -21,7 +21,33 @@ namespace collatzConjucture
 
         private void runButton_Click(object sender, EventArgs e)
         {
-            
+            moves = 0;
+            int numberEntered = 1;
+            int.TryParse(numberTextBox.Text, out numberEntered);
+            if (numberEntered < 2)
+            {
+                resultLabel.Text = "Enter a number greater than 1";
+            }
+            else
+            {
+                while (numberEntered != 1)
+                {
+                    if (IsOdd(numberEntered))
+                    {
+                        numberEntered = (numberEntered * 3) + 1;
+                        Console.WriteLine(numberEntered.ToString());
+                        moves += 1;
+                    }
+                    else
+                    {
+                        numberEntered/=2;
+                        Console.WriteLine(numberEntered.ToString());
+                        moves += 1;
+                    }
+                }
+                Console.WriteLine(moves.ToString());
+                resultLabel.Text = moves.ToString();
+            }
         }
         
         public static bool IsOdd(int value)
