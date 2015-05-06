a53164bc69a8fa6b3a09fd4c217834aae23ccfb1
Alexander Nohe <alex.nohe427@gmail.com>
2015-05-06 08:49:28 -0700

    Adding Endpoints to a line sample

---
+++ EndPointsAddin/EndPointsAddin/AddsTheData.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Reflection;
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.Geodatabase;
+
+namespace EndPointsAddin
+{
+    public class AddsTheData : ESRI.ArcGIS.Desktop.AddIns.Extension
+    {
+        public AddsTheData()
+        {
+        }
+
+        protected override void OnStartup()
+        {
+            //
+            // TODO: Uncomment to start listening to document events
+            //
+            WireDocumentEvents();
+        }
+
+        private void WireDocumentEvents()
+        {
+            //
+            // TODO: Sample document event wiring code. Change as needed
+            //
+
+            // Named event handler
+            ArcMap.Events.NewDocument += ArcMap_NewDocument;
+
+            
+            // Anonymous event handler
+            /*
+            ArcMap.Events.BeforeCloseDocument += delegate()
+            {
+                // Return true to stop document from closing
+                ESRI.ArcGIS.Framework.IMessageDialog msgBox = new ESRI.ArcGIS.Framework.MessageDialogClass();
+                return msgBox.DoModal("BeforeCloseDocument Event", "Abort closing?", "Yes", "No", ArcMap.Application.hWnd);
+            };
+             * 
+             */
+
+        }
+
+        private void ArcMap_NewDocument()
+        {
+            // TODO: Handle new document event
+
+            IMap map = ArcMap.Document.FocusMap;
+
+            Type factoryType = Type.GetTypeFromProgID("esriDataSourcesGDB.FileGDBWorkspaceFactory");
+            IWorkspaceFactory workspaceFactory = Activator.CreateInstance(factoryType) as IWorkspaceFactory;
+            IWorkspace workspace = workspaceFactory.OpenFromFile(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ,@"Data\PointsToAdd.gdb"), 0);
+            IFeatureWorkspace featureWorkspace = workspace as IFeatureWorkspace;
+            IFeatureClass featureClassLines = featureWorkspace.OpenFeatureClass("Lines");
+            IFeatureClass featureClassPoints = featureWorkspace.OpenFeatureClass("Points");
+
+            IFeatureLayer featureLayerLines = new FeatureLayer();
+            IFeatureLayer featureLayerPoints = new FeatureLayer();
+
+            featureLayerLines.FeatureClass = featureClassLines;
+            featureLayerPoints.FeatureClass = featureClassPoints;
+            featureLayerPoints.Name = "Points";
+            featureLayerLines.Name = "Lines";
+
+            map.AddLayer(featureLayerPoints);
+            map.AddLayer(featureLayerLines);
+            map.RecalcFullExtent();
+            
+        }
+
+    }
+
+}
---
+++ EndPointsAddin/EndPointsAddin/AddsThePoints.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.Editor;
+using ESRI.ArcGIS.Geodatabase;
+using ESRI.ArcGIS.Geometry;
+
+namespace EndPointsAddin
+{
+    /// <summary>
+    /// AddsThePoints class implementing custom ESRI Editor Extension functionalities.
+    /// </summary>
+    public class AddsThePoints : ESRI.ArcGIS.Desktop.AddIns.Extension
+    {
+        private IEditor theEditor;
+        public AddsThePoints()
+        {
+        }
+
+        protected override void OnStartup()
+        {
+            theEditor = ArcMap.Editor;
+            WireEditorEvents();
+        }
+
+        protected override void OnShutdown()
+        {
+        }
+
+        #region Editor Events
+
+        #region Shortcut properties to the various editor event interfaces
+        private IEditEvents_Event Events
+        {
+            get { return ArcMap.Editor as IEditEvents_Event; }
+        }
+        private IEditEvents2_Event Events2
+        {
+            get { return ArcMap.Editor as IEditEvents2_Event; }
+        }
+        private IEditEvents3_Event Events3
+        {
+            get { return ArcMap.Editor as IEditEvents3_Event; }
+        }
+        private IEditEvents4_Event Events4
+        {
+            get { return ArcMap.Editor as IEditEvents4_Event; }
+        }
+        #endregion
+
+        void WireEditorEvents()
+        {
+            //
+            //  TODO: Sample code demonstrating editor event wiring
+            //
+
+            //Events.OnCurrentTaskChanged += delegate
+            //{
+            //    if (ArcMap.Editor.CurrentTask != null)
+            //        System.Diagnostics.Debug.WriteLine(ArcMap.Editor.CurrentTask.Name);
+            //};
+
+            //Events2.BeforeStopEditing += delegate(bool save) { OnBeforeStopEditing(save); };
+
+            Events.OnCreateFeature += new IEditEvents_OnCreateFeatureEventHandler(OnCreationFinishedGenesis);
+        }
+
+        private void OnCreationFinishedGenesis(IObject obj)
+        {
+            ISpatialFilter spatialFilter = new SpatialFilter();
+            spatialFilter.SpatialRel = esriSpatialRelEnum.esriSpatialRelIntersects;
+
+            
+            
+            IGeometry geometryOfFeature = ((IFeature) obj).Shape;
+            if (geometryOfFeature.GeometryType == esriGeometryType.esriGeometryPolyline)
+            {
+                IPoint pointStart = ((IPolyline) geometryOfFeature).ToPoint;
+                IPoint pointEnd = ((IPolyline) geometryOfFeature).FromPoint;
+                //MessageBox.Show(String.Format("The starting X: {0} and y:{1}", pointStart.X, pointStart.Y));
+                //MessageBox.Show(String.Format("The ending X: {0} and y:{1}", pointEnd.X, pointEnd.Y));
+
+                IFeatureLayer pointLayer = (IFeatureLayer) GetPointFeature();
+
+
+                ITable table = ((IFeature) obj).Table;
+                IFeatureClass fc = table as IFeatureClass;
+                if ((fc.AliasName) == "Lines")
+                {
+                    spatialFilter.Geometry = pointStart;
+                    spatialFilter.GeometryField = fc.ShapeFieldName;
+
+                    IFeatureCursor allPoints = (pointLayer.FeatureClass).Search(spatialFilter, false);
+                    IFeature point = null;
+                    if (allPoints.NextFeature() == null)
+                    {
+                        IFeature featureInsert = (pointLayer.FeatureClass).CreateFeature();
+                        featureInsert.Shape = pointStart;
+                        featureInsert.Store();
+                    }
+
+                    spatialFilter.Geometry = pointEnd;
+                    spatialFilter.GeometryField = fc.ShapeFieldName;
+
+                    allPoints = (pointLayer.FeatureClass).Search(spatialFilter, false);
+                    point = null;
+                    if (allPoints.NextFeature() == null)
+                    {
+                        IFeature featureInsert = (pointLayer.FeatureClass).CreateFeature();
+                        featureInsert.Shape = pointEnd;
+                        featureInsert.Store();
+                        
+                    }
+                    allPoints.Flush();
+
+                    //spatialFilter.Geometry = ((IFeature) pointLayer.FeatureClass).ShapeCopy;
+                    //spatialFilter.GeometryField = fc.ShapeFieldName;
+
+                    //IFeatureCursor featureCursor = ((IFeatureClass) pointStart).Search(spatialFilter, false);
+                    //IFeature pointsInCursor = null;
+                    //while ((pointsInCursor = featureCursor.NextFeature()) != null)
+                    //{
+                    //    MessageBox.Show(pointsInCursor.get_Value(0));
+                    //}
+                }
+            }
+
+        }
+
+        private ILayer GetPointFeature()
+        {
+            ILayer layer = new FeatureLayer();
+            IMap map = ArcMap.Document.FocusMap;
+            for (int i = 0; i < map.LayerCount; i++)
+            {
+                if (map.Layer[i].Name == "Points")
+                
+                    layer = map.Layer[i];
+                
+            }
+            return layer;
+        }
+
+        void OnBeforeStopEditing(bool save)
+        {
+            
+        }
+        #endregion
+
+    }
+
+}
