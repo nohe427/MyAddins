ccbb9d5a0344e260428a38cb0ffca26f83b12004
Alexander Nohe <alex.nohe427@gmail.com>
2015-06-28 22:38:01 -0700

    Playing with XCode
    Xcode tests

---
+++ untitled folder/Test.cpp
@@ -0,0 +1,9 @@
+//
+//  Test.cpp
+//  
+//
+//  Created by Alexander Nohe on 6/28/15.
+//
+//
+
+#include "Test.h"
---
+++ untitled folder/Test.h
@@ -0,0 +1,14 @@
+//
+//  Test.h
+//  
+//
+//  Created by Alexander Nohe on 6/28/15.
+//
+//
+
+#ifndef ____Test__
+#define ____Test__
+
+#include <stdio.h>
+
+#endif /* defined(____Test__) */
