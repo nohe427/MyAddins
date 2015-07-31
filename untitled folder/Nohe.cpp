92cf0a0ca102389bcaf6f658aaf28755fbbc1e51
Alexander Nohe <alex.nohe427@gmail.com>
2015-07-30 19:46:47 -0700

    New additions...

---
+++ untitled folder/Nohe.cpp
@@ -0,0 +1,17 @@
+//
+//  Nohe.cpp
+//  
+//
+//  Created by Alexander Nohe on 6/30/15.
+//
+//
+
+#include "Nohe.h"
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello world!";
+    std::Hello;
+    return 0;
+}
---
+++ untitled folder/TestFile.h
@@ -0,0 +1,14 @@
+//
+//  TestFile.h
+//  
+//
+//  Created by Alexander Nohe on 7/26/15.
+//
+//
+
+#ifndef ____TestFile__
+#define ____TestFile__
+
+#include <stdio.h>
+
+#endif /* defined(____TestFile__) */
